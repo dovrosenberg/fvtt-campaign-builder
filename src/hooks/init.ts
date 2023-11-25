62cbf160bda4fd25c86cbc2c2ce3b19f15901627
Dov Rosenberg <dov@rosenbergmail.com>
2023-11-25 12:25:38 -0500

    cleanup

--- src/hooks/init.ts
+++ src/hooks/init.ts
@@ -1,5 +1,7 @@
 import { ModuleSettings, updateModuleSettings } from '@/settings/ModuleSettings';
 
+let autocompleter = null as Application | null;
+
 export function registerForInitHook() {
   Hooks.once('init', init);
 }
@@ -7,4 +9,19 @@ export function registerForInitHook() {
 async function init(): Promise<void> {
   // initialize settings first, so other things can use them
   updateModuleSettings(new ModuleSettings());
+
+  alert('here');
+  
 }
+
+// function activateAutocompleter(targetElement) {
+//   autocompleter?.close();
+
+//   // Otherwise, create a new autocompleter
+//   autocompleter = new Autocompleter(targetElement, () => {
+//       // When this Autocompleter gets closed, clean up the registration for this element.
+//       autocompleter = null;
+//   });
+  
+//   autocompleter.render(true);
+// }
