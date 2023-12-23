83b100f91ed2667d6443acf6155a2f5fb1119d06
Dov Rosenberg <dov@rosenbergmail.com>
2023-12-23 10:45:24 -0500

    WIP

--- src/components/Editor/EditorJournalSheet.ts
+++ src/components/Editor/EditorJournalSheet.ts
@@ -2,6 +2,9 @@
 export class EditorJournalPage extends JournalTextPageSheet {
   constructor(...args) { 
     super(...args);
+
+    // whenever we have one of these, treat is as rendered
+    this._state = this.constructor.RENDER_STATES.RENDERED;
   }
 
   get isEditable() {
@@ -14,5 +17,13 @@ export class EditorJournalPage extends JournalTextPageSheet {
 
   public activateListeners(html: JQuery) {
     super.activateListeners(html);
+  }
+
+  public async saveEditor(name: string, remove?: boolean): Promise<void> {
+    debugger;
+
+    super.saveEditor(name, remove);
+
+    // probably raise an event up to parent to update???  or maybe just unlock the compendia, do the save with super() and relock
   }
 }
