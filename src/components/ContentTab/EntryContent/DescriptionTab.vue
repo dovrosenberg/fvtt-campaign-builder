136c9bc23d5cecb142266fe8eb8453411136adbf
Dov Rosenberg <dov@rosenbergmail.com>
2025-04-01 07:00:07 -0400

    reformatting WIP - need to move functions, bubble events, and fix margins

---
+++ src/components/ContentTab/EntryContent/DescriptionTab.vue
@@ -0,0 +1,169 @@
+<template>
+  <div class="wcb-journal-sheet-header flexrow">
+    <ImagePicker
+      v-model="entryImg"
+      :title="`Select Image for ${currentEntry?.name || 'Entry'}`"
+    />        
+    <div class="wcb-content-header">
+      <div class="flexrow form-group">
+        <label>{{ localize('labels.fields.type') }}</label>
+        <TypeSelect
+          :initial-value="currentEntry?.type || ''"
+          :topic="props.topic"
+          @type-selection-made="onTypeSelectionMade"
+        />
+      </div>
+
+      <!-- show the species for characters -->
+      <div 
+        v-if="topic===Topics.Character"
+        class="flexrow form-group"
+      >
+        <label>{{ localize('labels.fields.species') }}</label>
+        <SpeciesSelect
+          :initial-value="currentEntry?.speciesId || ''"
+          :allow-new-items="false"
+          @species-selection-made="onSpeciesSelectionMade"
+        />
+      </div>
+
+      <div 
+        v-if="showHierarchy"
+        class="flexrow form-group"
+      >
+        <label>{{ localize('labels.fields.parent') }}</label>
+        <TypeAhead 
+          :initial-list="validParents"
+          :initial-value="props.parentId || ''"
+          @selection-made="onParentSelectionMade"
+        />
+      </div>
+
+      <Editor 
+        :initial-content="currentEntry?.description || ''"
+        :has-button="true"
+        @editor-saved="onDescriptionEditorSaved"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+  // library imports
+  import { computed, PropType } from 'vue';
+  import { storeToRefs, } from 'pinia';
+
+  // local imports
+  import { hasHierarchy, } from '@/utils/hierarchy';
+  import { useMainStore, useTopicDirectoryStore } from '@/applications/stores';
+  import { localize } from '@/utils/game';
+
+  // library components
+
+  // local components
+  import Editor from '@/components/Editor.vue';
+  import TypeAhead from '@/components/TypeAhead.vue';
+  import SpeciesSelect from '@/components/ContentTab/EntryContent/SpeciesSelect.vue';
+  import TypeSelect from '@/components/ContentTab/EntryContent/TypeSelect.vue';
+  import ImagePicker from '@/components/ImagePicker.vue'; 
+
+  // types
+  import { ValidTopic, Topics, } from '@/types';
+  
+  ////////////////////////////////
+  // props
+  const props = defineProps({
+    validParents: {
+      type: Array as PropType<{id: string; label: string}[]>,
+      required: false,
+      default: [],
+    },
+    topic: {
+      type: Number as PropType<ValidTopic>,
+      required: true,
+    },
+    parentId: {
+      type: String,
+      required: false,
+      default: '',
+    }
+  });
+
+  ////////////////////////////////
+  // emits
+
+  ////////////////////////////////
+  // store
+  const mainStore = useMainStore();
+  const topicDirectoryStore = useTopicDirectoryStore();
+  const { currentEntry, } = storeToRefs(mainStore);
+
+  ////////////////////////////////
+  // data
+  const defaultImage = 'icons/svg/mystery-man.svg'; // Default Foundry image
+
+  ////////////////////////////////
+  // computed data
+  const showHierarchy = computed((): boolean => (props.topic===null ? false : hasHierarchy(props.topic)));
+  const entryImg = computed({
+    get: (): string => currentEntry.value?.img || defaultImage,
+    set: async (value: string) => {
+      if (currentEntry.value) {
+        currentEntry.value.img = value;
+        await currentEntry.value.save();
+      }
+    }
+  });
+
+  ////////////////////////////////
+  // methods
+
+  ////////////////////////////////
+  // event handlers
+  const onTypeSelectionMade = async (selection: string) => {
+    if (currentEntry.value) {
+      const oldType = currentEntry.value.type;
+      currentEntry.value.type = selection;
+      await currentEntry.value.save();
+
+      await topicDirectoryStore.updateEntryType(currentEntry.value, oldType);
+    }
+  };
+
+  const onParentSelectionMade = async (selection: string): Promise<void> => {
+    if (!currentEntry.value?.topic || !currentEntry.value?.uuid)
+      return;
+
+    if (!currentEntry.value.topicFolder)
+      throw new Error('Invalid topic in EntryContent.onParentSelectionMade()');
+
+    await topicDirectoryStore.setNodeParent(currentEntry.value.topicFolder, currentEntry.value.uuid, selection || null);
+  };
+
+  const onDescriptionEditorSaved = async (newContent: string) => {
+    if (!currentEntry.value)
+      return;
+
+    currentEntry.value.description = newContent;
+    await currentEntry.value.save();
+  };
+
+  const onSpeciesSelectionMade = async (species: {id: string; label: string}): Promise<void> => {
+    if (!currentEntry.value?.topic || !currentEntry.value?.uuid)
+      return;
+
+    currentEntry.value.speciesId = species.id;
+    await currentEntry.value.save();
+  };
+
+
+  ////////////////////////////////
+  // watchers
+
+  ////////////////////////////////
+  // lifecycle events
+
+</script>
+
+<style lang="sass">
+</style>
--- src/components/ContentTab/EntryContent/EntryContent.vue
+++ src/components/ContentTab/EntryContent/EntryContent.vue
@@ -2,10 +2,6 @@
   <form :class="'flexcol wcb-journal-subsheet ' + topic">
     <div ref="contentRef" class="wcb-sheet-container flexcol">
       <header class="wcb-journal-sheet-header flexrow">
-        <ImagePicker
-          v-model="entryImg"
-          :title="`Select Image for ${currentEntry?.name || 'Entry'}`"
-        />        
         <div class="wcb-content-header">
           <h1 class="header-name flexrow">
             <i :class="`fas ${icon} sheet-icon`"></i>
@@ -30,39 +26,6 @@
               <i class="fas fa-head-side-virus"></i>
             </button>
           </h1>
-          <div class="flexrow form-group">
-            <label>{{ localize('labels.fields.type') }}</label>
-            <TypeSelect
-              :initial-value="currentEntry?.type || ''"
-              :topic="topic as ValidTopic"
-              @type-selection-made="onTypeSelectionMade"
-            />
-          </div>
-
-          <!-- show the species for characters -->
-          <div 
-            v-if="topic===Topics.Character"
-            class="flexrow form-group"
-          >
-            <label>{{ localize('labels.fields.species') }}</label>
-            <SpeciesSelect
-              :initial-value="currentEntry?.speciesId || ''"
-              :allow-new-items="false"
-              @species-selection-made="onSpeciesSelectionMade"
-            />
-          </div>
-
-          <div 
-            v-if="showHierarchy"
-            class="flexrow form-group"
-          >
-            <label>{{ localize('labels.fields.parent') }}</label>
-            <TypeAhead 
-              :initial-list="validParents"
-              :initial-value="parentId || ''"
-              @selection-made="onParentSelectionMade"
-            />
-          </div>
         </div>
       </header>
       <nav class="wcb-sheet-navigation flexrow tabs" data-group="primary">
@@ -92,11 +55,13 @@
       </nav>
       <div class="wcb-tab-body flexcol">
         <div class="tab description flexcol" data-group="primary" data-tab="description">
-          <div class="tab-inner flexcol">
-            <Editor 
-              :initial-content="currentEntry?.description || ''"
-              :has-button="true"
-              @editor-saved="onDescriptionEditorSaved"
+          <div v-if="topic"
+            class="tab-inner flexcol"
+          >
+            <DescriptionTab 
+              :valid-parents="validParents"
+              :parent-id="parentId || undefined"
+              :topic="topic"
             />
           </div>
         </div>
@@ -160,27 +125,23 @@
   // local imports
   import { getTopicIcon, htmlToPlainText } from '@/utils/misc';
   import { localize } from '@/utils/game';
-  import { hasHierarchy, validParentItems, } from '@/utils/hierarchy';
   import { useTopicDirectoryStore, useMainStore, useNavigationStore, useRelationshipStore, } from '@/applications/stores';
   import { Backend } from '@/classes/Backend';
   import { ModuleSettings, SettingKey } from '@/settings';
-  
+  import { validParentItems, } from '@/utils/hierarchy';
+
   // library components
   import InputText from 'primevue/inputtext';
   import ContextMenu from '@imengyu/vue3-context-menu';
 
   // local components
-  import Editor from '@/components/Editor.vue';
-  import TypeAhead from '@/components/TypeAhead.vue';
+  import DescriptionTab from './DescriptionTab.vue';
   import RelatedItemTable from '@/components/ItemTable/RelatedItemTable.vue';
   import RelatedDocumentTable from '@/components/DocumentTable/RelatedDocumentTable.vue';
-  import SpeciesSelect from '@/components/ContentTab/EntryContent/SpeciesSelect.vue';
-  import TypeSelect from '@/components/ContentTab/EntryContent/TypeSelect.vue';
   import GenerateDialog from '@/components/AIGeneration/GenerateDialog.vue';
-  import ImagePicker from '@/components/ImagePicker.vue'; 
 
   // types
-  import { DocumentLinkType, Topics, ValidTopic, GeneratedCharacterDetails, Species, GeneratedLocationDetails, GeneratedOrganizationDetails } from '@/types';
+  import { DocumentLinkType, Topics, GeneratedCharacterDetails, Species, GeneratedLocationDetails, GeneratedOrganizationDetails } from '@/types';
   import { Entry, WBWorld, TopicFolder } from '@/classes';
 
   ////////////////////////////////
@@ -222,22 +183,11 @@
   const validParents = ref<{id: string; label: string}[]>([]);
   const showGenerate = ref<boolean>(false);
   const isGeneratingImage = ref<boolean>(false); // Flag to track whether image generation is in progress
-  const defaultImage = 'icons/svg/mystery-man.svg'; // Default Foundry image
   
   ////////////////////////////////
   // computed data
   const icon = computed((): string => (!topic.value ? '' : getTopicIcon(topic.value)));
-  const showHierarchy = computed((): boolean => (topic.value===null ? false : hasHierarchy(topic.value)));
   const namePlaceholder = computed((): string => (topic.value===null ? '' : (localize(topicData[topic.value]?.namePlaceholder || '') || '')));
-  const entryImg = computed({
-    get: (): string => currentEntry.value?.img || defaultImage,
-    set: async (value: string) => {
-      if (currentEntry.value) {
-        currentEntry.value.img = value;
-        await currentEntry.value.save();
-      }
-    }
-  });
   const canGenerate = computed(() => topic.value && [Topics.Character, Topics.Location, Topics.Organization].includes(topic.value));
   const generateDisabled = computed(() => !Backend.available);
 
@@ -293,42 +243,6 @@
         await relationshipStore.propagateNameChange(currentEntry.value);
       }
     }, debounceTime);
-  };
-
-  const onTypeSelectionMade = async (selection: string) => {
-    if (currentEntry.value) {
-      const oldType = currentEntry.value.type;
-      currentEntry.value.type = selection;
-      await currentEntry.value.save();
-
-      await topicDirectoryStore.updateEntryType(currentEntry.value, oldType);
-    }
-  };
-
-  const onParentSelectionMade = async (selection: string): Promise<void> => {
-    if (!currentEntry.value?.topic || !currentEntry.value?.uuid)
-      return;
-
-    if (!currentEntry.value.topicFolder)
-      throw new Error('Invalid topic in EntryContent.onParentSelectionMade()');
-
-    await topicDirectoryStore.setNodeParent(currentEntry.value.topicFolder, currentEntry.value.uuid, selection || null);
-  };
-
-  const onDescriptionEditorSaved = async (newContent: string) => {
-    if (!currentEntry.value)
-      return;
-
-    currentEntry.value.description = newContent;
-    await currentEntry.value.save();
-  };
-
-  const onSpeciesSelectionMade = async (species: {id: string; label: string}): Promise<void> => {
-    if (!currentEntry.value?.topic || !currentEntry.value?.uuid)
-      return;
-
-    currentEntry.value.speciesId = species.id;
-    await currentEntry.value.save();
   };
 
   const onGenerateButtonClick = (event: MouseEvent): void => {
