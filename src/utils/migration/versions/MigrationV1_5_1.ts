d984ceb0d5ddf4d023a393ca7de4c158b62935db
Dov Rosenberg <dov@rosenbergmail.com>
2025-11-01 20:58:22 -0400

    migration works including bug fix - next do campaigns

--- src/utils/migration/versions/MigrationV1_5_1.ts
+++ src/utils/migration/versions/MigrationV1_5_1.ts
@@ -1,7 +1,7 @@
 import { Migration, MigrationResult, MigrationContext } from '../types';
 import { notifyError } from '@/utils/notifications';
 import { useMainStore } from '@/applications/stores';
-import { EntryBasicIndex } from '@/types';
+import { EntryBasicIndex, Hierarchy, Topics, ValidTopic } from '@/types';
 
 
 let processed = 0;
@@ -36,12 +36,6 @@ export class MigrationV1_5_1 implements Migration {
       warnings: []
     };
 
-    // if we just ran the 1.5.0 then we don't need to do anything because it applies
-    //   the latest model; but if we are upgrading from 1.5.0 previously migrated, then
-    //   we need to do this
-    if (this._context.originalVersion !== '1.5.0')
-      return result;
-
     try {
       const settings = await useMainStore().getAllSettings();
 
@@ -52,21 +46,79 @@ export class MigrationV1_5_1 implements Migration {
         }
       }
 
+      // double it for hierarchies plus actual entries (unless we're going to skip the actual ones)
+      if (this._context.originalVersion === '1.5.0')
+        totalEntries *= 2;
+
+      const getAncestors = (hierarchies: Record<string, Hierarchy>, entryId: string): string[] => {
+        const entry = hierarchies[entryId];
+        if (!entry)
+          return [];
+
+        if (entry.parentId) {
+          return [entry.parentId, ...getAncestors(hierarchies, entry.parentId)];
+        } else {
+          return [];
+        }
+      };
+
+      // rebuild the hierarchies... the parents are right but the ancestors aren't
+      for (const setting of settings) {
+        const updatedHierarchies = {};
+
+        const hierarchies = setting.hierarchies;
+
+        // for every element, do a recursive search for parents to fix the ancestor list
+        for (const entryId in hierarchies) {
+          const entry = hierarchies[entryId];
+
+          if (!entry)
+            continue;
+
+          updatedHierarchies[entryId] = {
+            ...entry,
+            ancestors: getAncestors(hierarchies, entryId)
+          };
+
+          processed++;
+          updateProgress(`Processing hierarchy`);
+        }
+
+        setting.hierarchies = updatedHierarchies;
+        await setting.save();
+      }
+
+      // if we just ran the 1.5.0 then we don't need to update the entry structure because it applies
+      //   the latest model; but if we are upgrading from 1.5.0 previously migrated, then
+      //   we need to do this
+      if (this._context.originalVersion !== '1.5.0')
+        return result;
+
       // now do the conversion
       for (const setting of settings) {
-        for (const topicId in setting.topics) {
-          const newEntries = [] as EntryBasicIndex[];
+        const newEntries = {
+          [Topics.Character]: [] as EntryBasicIndex[],
+          [Topics.Location]: [] as EntryBasicIndex[],
+          [Topics.Organization]: [] as EntryBasicIndex[],
+          [Topics.PC]: [] as EntryBasicIndex[],
+        } as Record<ValidTopic, EntryBasicIndex[]>;
 
-            for (const entryId in setting.topics[topicId].entries) {
-            newEntries.push(await migrateEntry(entryId));
+        const entries = await setting.allEntries(true);
 
-            processed++;
-            updateProgress(`Processing entry: ${newEntries[newEntries.length-1].name}`);
-          }
-
-          setting.topics[topicId].entries = newEntries;
-          await setting.save();
+        for (const entry of entries) {
+          newEntries[entry.topic].push({
+            uuid: entry.uuid,
+            name: entry.name,
+            type: entry.type
+          });
+          processed++;
+          updateProgress(`Processing entry: ${entry.name}`);
         }
+
+        for (const topicId in setting.topics) {
+          setting.topics[topicId].entries = newEntries[topicId];
+        }
+        await setting.save();
       }
     } catch (outer) {
       result.success = false;
@@ -79,18 +131,3 @@ export class MigrationV1_5_1 implements Migration {
     return result;
   }    
 }
-
-async function migrateEntry(uuid: string): Promise<EntryBasicIndex> {
-  const entry = await fromUuid<JournalEntry>(uuid);
-  
-  if (!entry)
-    throw new Error(`Unable to find entry for ${uuid} in migrateEntry`);
-
-  return {
-    uuid,
-    name: entry.name,
-    // @ts-ignore
-    type: entry.system?.type || ''
-  }
-}
-
