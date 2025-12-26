3265d1d517cc2c70a224fa907d4f654b61fbdaf0
Dov Rosenberg <dov@rosenbergmail.com>
2025-12-25 19:32:01 -0500

    migration working

--- src/utils/migration/versions/MigrationV1_8.ts
+++ src/utils/migration/versions/MigrationV1_8.ts
@@ -44,24 +44,25 @@ export class MigrationV1_8 implements Migration {
     try {
       // we have to temporarily register these settings because we need to know the old value
       // @ts-ignore
+      debugger;
       game.settings.register(moduleId, 'showRolePlayingNotes', {
         default: true,
         type: Boolean,
-        scope: 'client',
+        scope: 'world',
         config: false,
       });
       // @ts-ignore
       game.settings.register(moduleId, 'rpgStyle', {
         default: true,
         type: Boolean,
-        scope: 'client',
+        scope: 'world',
         config: false,
       });
       // @ts-ignore
       game.settings.register(moduleId, 'longDescriptionParagraphs', {
         default: 2,
         type: Number,
-        scope: 'client',
+        scope: 'world',
         config: false,
       });
        
@@ -97,6 +98,7 @@ export class MigrationV1_8 implements Migration {
 
         // if rpgstyle was off, it was a long description; if it was on, it was boxed text
         // @ts-ignore
+        debugger;
         if (!game.settings.get(moduleId, 'rpgStyle')) {
           // @ts-ignore
           const longDescriptionParagraphs = game.settings.get(moduleId, 'longDescriptionParagraphs') as Number || 2;
