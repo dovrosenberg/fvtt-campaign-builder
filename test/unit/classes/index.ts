613829b7bced1cc855fddcc8f99e70d3eeb36757
Dov Rosenberg <dov@rosenbergmail.com>
2026-03-28 09:53:45 -0400

    tests

--- test/unit/classes/index.ts
+++ test/unit/classes/index.ts
@@ -1,8 +1,14 @@
 import { createBatch } from '@unittest/testUtils';
 import { registerEntryTests } from './Entry.test';
+import { registerFCBJournalEntryPageTests } from './FCBJournalEntryPage.test';
+import { registerFCBSettingTests } from './FCBSetting.test';
+import { registerCampaignTests } from './Campaign.test';
 
 export const registerClassBatches = () => {
   registerEntryBatch();
+  registerFCBJournalEntryPageBatch();
+  registerFCBSettingBatch();
+  registerCampaignBatch();
 };
 
 
@@ -11,5 +17,29 @@ const registerEntryBatch = () => {
     'campaign-builder.classes.Entry',
     '/classes/Entry',
     registerEntryTests
+  );
+};
+
+const registerFCBJournalEntryPageBatch = () => {
+  createBatch(
+    'campaign-builder.classes.FCBJournalEntryPage',
+    '/classes/FCBJournalEntryPage',
+    registerFCBJournalEntryPageTests
+  );
+};
+
+const registerFCBSettingBatch = () => {
+  createBatch(
+    'campaign-builder.classes.FCBSetting',
+    '/classes/FCBSetting',
+    registerFCBSettingTests
+  );
+};
+
+const registerCampaignBatch = () => {
+  createBatch(
+    'campaign-builder.classes.Campaign',
+    '/classes/Campaign',
+    registerCampaignTests
   );
 };
